e7927948c33f16813ef90dbee1ba722773418d5d
VIVEK  ARORA <73037125+vivekarora-02@users.noreply.github.com>
2021-10-03 16:41:32 +0530

    Create arrays.cpp

---
+++ Data-Structures/arrays.cpp
@@ -0,0 +1,40 @@
+// 1-d arrays
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Array declaration and initialization
+    int arr[5] = {4, 12, 7, 15, 9};
+    // Iterate over the array
+    for(int idx=0; idx<5; idx++)
+    {
+        // Print out each element in a new line
+        cout << arr[idx] << endl;
+    }
+    return 0;
+}
+
+// 2-d arrays
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Array declaration and initialization
+    int arr[3][5] = {{5, 12, 17, 9, 3}, {13, 4, 8, 14, 1}, {9, 6, 3, 7, 21}};
+    // Iterate over the array
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            // Print out each element
+            cout << arr[i][j];
+        }
+        // Print new line character after the row is printed in above loop
+        cout << endl;
+    }
+    return 0;
+}
